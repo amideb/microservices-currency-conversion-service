60411a88ced3eadc313be8a1825abc81f3b9e666
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-07 20:16:50 +0530

    controller-updated

--- src/main/java/com/debrup/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/debrup/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,2 +1,23 @@
-package com.debrup.currencyconversionservice.controller;public class CurrencyConversionController {
+package com.debrup.currencyconversionservice.controller;
+
+import com.debrup.currencyconversionservice.entity.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+            ){
+
+        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "" );
+    }
+
 }
